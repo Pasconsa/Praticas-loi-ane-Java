0a7655970814ac5981995ab66c07db6eeedc47a7
pasconsa <saulopascon@gmail.com>
2022-08-30 18:53:37 -0300

    Exercicios 3 a 5

---
+++ src/aula17/Exe03.java
@@ -0,0 +1,101 @@
+package aula17;
+
+import java.util.Scanner;
+
+import javax.xml.stream.util.EventReaderDelegate;
+
+/*
+ * Faça um programa que leia e valide as seguintes informações:
+a. Nome: maior que 3 caracteres;
+b. Idade: entre 0 e 150;
+c. Salário: maior que zero;
+d. Sexo: 'f' ou 'm';
+e. Estado Civil: 's', 'c', 'v', 'd';
+ */
+
+public class Exe03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		boolean infoValida = false;
+		
+		do {
+			System.out.println("Entre com o nome ");
+			String nome = sc.next();
+			
+			if(nome.length() >= 3) {
+				infoValida = true;
+			}else {
+				System.out.println("Nome precisa no minimo de 3 caracteres.");
+			}
+		}while(!infoValida);
+		
+		
+		
+		infoValida = false;
+		
+		do {
+			System.out.println("Entre com uma idade ");
+			double idade = sc.nextDouble();
+			
+			if(idade >=0 && idade <=150) {
+				infoValida = true;
+			}else {
+				System.out.println("escolha uma idade entre 0 a 150.");
+			}
+		}while(!infoValida);
+		
+	
+	
+
+	
+infoValida = false;
+	
+	do {
+		System.out.println("Digite o salario:");
+		double salario = sc.nextDouble();
+		
+		if(salario > 0) {
+			infoValida = true;
+		}else {
+			System.out.println("salario não pode ser menor que zero");
+		}
+	}while(!infoValida);
+	
+	
+infoValida = false;
+	
+	do {
+		System.out.println("Digite o sexo:");
+		String sexo = sc.next();
+		
+		if(sexo.equalsIgnoreCase("f")  || sexo.equalsIgnoreCase("m")) {
+			infoValida = true;
+		}else {
+			System.out.println("o sexo deve ser 'f' ou 'm'.");
+		}
+	}while(!infoValida);
+	
+	
+	
+	infoValida = false;
+	
+	do {
+		System.out.println("Digite a primeira letra do estado civil");
+		String estc = sc.next();
+		
+		if(estc.equalsIgnoreCase("s") || estc.equalsIgnoreCase("c") || estc.equalsIgnoreCase("v") || estc.equalsIgnoreCase("d")  ) {
+			infoValida = true;
+		}else {
+			System.out.println("digite a primeira letra do xeu estado civil.");
+		}
+	}while(!infoValida);
+	
+	
+	
+}
+	
+	
+}
---
+++ src/aula17/Exe05.java
@@ -0,0 +1,93 @@
+package aula17;
+
+import java.util.Scanner;
+
+/*5. Altere o programa anterior permitindo ao usuário informar as
+populações e as taxas de crescimento iniciais. Valide a entrada e
+permita repetir a operação*/
+
+public class Exe05 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int popA;
+		int popB;
+		double taxA;
+		double taxB;
+		double contAno=0;
+		
+
+		boolean valido = false;
+		do {
+
+			System.out.println("Digite o valor da populacao A: ");
+			popA = sc.nextInt();
+
+			if (popA > 0) {
+				valido = true;
+			} else {
+				System.out.println("População A precisa ser maior que zero");
+			}
+		} while (!valido);
+		
+		
+		
+		valido = false;
+		do {
+
+			System.out.println("Digite o valor da populacao B: ");
+			popB = sc.nextInt();
+
+			if (popB > 0) {
+				valido = true;
+			} else {
+				System.out.println("População B precisa ser maior que zero");
+			}
+		} while (!valido);
+		
+		
+		
+		valido = false;
+		do {
+
+			System.out.println("Digite o valor da taxa A: ");
+			taxA = sc.nextDouble();
+
+			if (taxA > 0) {
+				valido = true;
+			} else {
+				System.out.println("Taxa precisa ser maior que zero");
+			}
+		} while (!valido);
+		
+		
+		valido = false;
+		do {
+
+			System.out.println("Digite o valor da taxa B: ");
+			taxB = sc.nextDouble();
+
+			if (taxB > 0) {
+				valido = true;
+			} else {
+				System.out.println("Taxa precisa ser maior que zero");
+			}
+		} while (!valido);
+
+	
+
+		while (popA < popB) {
+
+			popA += (popA / 100) * taxA;
+			popB += (popB / 100) * taxB;
+			contAno++;
+		}
+
+		System.out.println("Populacao A: " + popA);
+		System.out.println("Populacao B: " + popB);
+		System.out.println("Quantos anos: " + contAno);
+
+	}
+}
