6d85b71f9c2b33285abe380a10df6aed64116af6
Pasconsa <saulopascon@hotmail.com>
2022-09-09 19:00:37 -0300

    ex018

---
+++ src/aula17/Ex019.java
@@ -0,0 +1,41 @@
+/*
+ * 21 Faça um programa que peça um número inteiro e determine se ele é
+ou não um número primo. Um número primo é aquele que é divisível
+somente por ele mesmo e por 1.
+ */
+
+package aula17;
+
+import java.io.InvalidObjectException;
+import java.util.Scanner;
+
+public class Ex019 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com o numero de turmas:");
+		int numTurmas = scan.nextInt();
+
+		int numAlunos;
+		int soma = 0;
+		boolean invalido = true;
+
+		for (int i = 1; i <=numTurmas; i++) {
+
+			invalido = true;
+			do {
+				System.out.println("Entre com numero de alunos da turma" + i);
+				numAlunos = scan.nextInt();
+
+				if (numAlunos <= 40) {
+					invalido = false;
+				} else {
+					System.out.println("Numero de alunos invalidos digite novamente");
+				}
+			} while (invalido);
+			soma += numAlunos;
+		}
+		double media = soma / numTurmas;
+		System.out.println("média: " + media);
+	}
+}
