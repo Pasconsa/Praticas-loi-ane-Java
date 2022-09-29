28936dd01526797a4829a30f2a6b2354d022c6f4
Pasconsa <saulopascon@hotmail.com>
2022-09-28 21:07:04 -0300

    Exe020 vetorese

---
+++ src/aula18VetoresArrays/Exe020.java
@@ -0,0 +1,15 @@
+/*
+ * 20.Implementar um programa que obtenha a cotação do dólar (U$) em
+		relação ao real (R$) 
+		e a seguir armazene em vetor A com 20 elementos as seguintes conversões:
+	21.A[i] = cotação do dolar * i, para todo i variando de 1 até 20.
+ */
+
+package aula18VetoresArrays;
+
+public class Exe020 {
+
+	public static void main(String[] args) {
+		
+	}
+}
