3247aed67584783672fda48e4af7a42d46385287
Pasconsa <saulopascon@hotmail.com>
2022-09-30 15:13:36 -0300

    Exe030

---
+++ src/aula18VetoresArrays/Exe030.java
@@ -0,0 +1,11 @@
+/*
+ * 30.Ler um vetor A com 20 elementos. Construir dois vetores B e C, sendo
+que nos vetor B e C serão armazenados o valores pares e ímpares de
+A, respectivamente.
+ */
+
+package aula18VetoresArrays;
+
+public class Exe030 {
+
+}
