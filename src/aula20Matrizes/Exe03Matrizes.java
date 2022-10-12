040c92c58f97747242bc96ee1ff828d71f97abf5
Pasconsa <saulopascon@hotmail.com>
2022-10-12 15:10:13 -0300

    Exe03Matriz

---
+++ src/aula20Matrizes/Exe03Matrizes.java
@@ -0,0 +1,11 @@
+/*
+ * 3. Capture do teclado valores para preenchimento de uma matriz M
+3x3. Após a captura imprima a matriz criada e encontre a
+quantidade de números pares, a quantidade de números ímpares.
+ */
+
+package aula20Matrizes;
+
+public class Exe03Matrizes {
+
+}
