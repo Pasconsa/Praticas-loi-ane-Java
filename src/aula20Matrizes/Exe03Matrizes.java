2cdcda029e2beb18cd34a4f55a862f2e38708aca
Pasconsa <saulopascon@hotmail.com>
2022-10-15 16:35:23 -0300

    Exe03Matriz

--- src/aula20Matrizes/Exe03Matrizes.java
+++ src/aula20Matrizes/Exe03Matrizes.java
@@ -13,5 +13,15 @@ public class Exe03Matrizes {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
+		int [] [] numeros = new int [3] [3];
+		
+		for (int i=0; i<numeros.length ; i++) {
+			for (int j=0; j< numeros[i].length ; j++) {
+				
+				System.out.println("Entre com o valor da pos [" + i + "," + j + "]");
+				numeros[i] [j] = sc.nextInt();
+			}
+		}
 	}
 }
