f814b919f7019092621838b116cf4e28556c673e
Pasconsa <saulopascon@hotmail.com>
2022-10-16 09:09:50 -0300

    Exe03.4

--- src/aula20Matrizes/Exe03Matrizes.java
+++ src/aula20Matrizes/Exe03Matrizes.java
@@ -23,5 +23,31 @@ public class Exe03Matrizes {
 				numeros[i] [j] = sc.nextInt();
 			}
 		}
+		
+		int qtdPares = 0;
+		int qtdImpares = 0;
+		
+		for (int i=0; i<numeros.length ; i++) {
+			for (int j=0; j< numeros[i].length ; j++) {
+				
+				if(numeros[i] [j] % 2 ==0 ) {
+					qtdPares++;
+		
+				}else {
+					qtdImpares++;
+				}
+			}
+		}
+		
+		for (int i=0; i<numeros.length ; i++) {
+			for (int j=0; j< numeros[i].length ; j++) {
+				System.out.print(numeros[i] [j] + " ");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println("pares = " + qtdPares);
+		System.out.println("Impares = " + qtdImpares);
 	}
 }
