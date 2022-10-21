c9d88770e09033b5894723be5d9e515b44a7a1a1
Pasconsa <saulopascon@hotmail.com>
2022-10-21 16:47:44 -0300

    ForEach

---
+++ src/aula17IfElseWhile/Exe00ForEach.java
@@ -0,0 +1,21 @@
+package aula17IfElseWhile;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exe00ForEach {
+	
+	public static void main(String[] args) {
+		
+
+		        List<Integer> itens = Arrays.asList( 11, 10, 16, 5, 85 );
+
+		        for(Integer item : itens){
+		            System.out.println(item);
+		        }
+		    }
+		}
+		
+	
+
+
