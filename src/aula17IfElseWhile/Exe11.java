855b5cb782a35bcd615104419136e5ad904a6046
Pasconsa <saulopascon@hotmail.com>
2022-12-02 10:51:32 -0300

    exe11

--- src/aula17IfElseWhile/Exe11.java
+++ src/aula17IfElseWhile/Exe11.java
@@ -8,25 +8,20 @@ public class Exe11 {
 
 		public static void main(String[] args) {
 			
-			Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner (System.in);
+		
+		int soma = 0;
 			
-			int n1;
-			int n2;
-			int soma=0;
+		System.out.println("Digite dois numeros");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		for(int i = n1 ; i <= n2; i++) {
+			soma += i;
 			
-			System.out.println("Entre com o primeiro numero");
-			n1 = sc.nextInt();
-			
-			System.out.println("Entre com o segundo numero");
-			n2 = sc.nextInt();
-			
-			for (int i=n1; i<=n2; i++) {
-				soma += i;
-				
-			}System.out.println("Soma " + soma);
+		
+		}
+		System.out.println(soma);
 		}
 		
-	
-
-	
 }
